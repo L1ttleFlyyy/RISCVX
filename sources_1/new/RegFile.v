b77ac6612d25cbd59312fe80a9765e2bf45d6847
Chang Xu <920114525@qq.com>
2019-07-31 18:47:05 -0700

    reg file finished

--- sources_1/new/RegFile.v
+++ sources_1/new/RegFile.v
@@ -21,6 +21,23 @@
 
 
 module RegFile(
-
+    input clk,
+    input [4:0] rs1,
+    input [4:0] rs2,
+    input [4:0] rd,
+    input regwrite,
+    input [31:0] rd_data,
+    output [31:0] rs1_data,
+    output [31:0] rs2_data
     );
+
+    reg [31:0] regs [1:31];
+    assign rs1_data = (rs1 == 5'b0)? 32'b0 : ((rd == rs1) && regwrite) ? rd_data : regs[rs1];
+    assign rs2_data = (rs2 == 5'b0)? 32'b0 : ((rd == rs2) && regwrite) ? rd_data : regs[rs2];
+
+    always@(posedge clk) begin
+        if (regwrite)
+            regs[rd] <= rd_data;
+    end
+
 endmodule
