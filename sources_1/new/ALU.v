c8167d07cd137c50fb9f7ea3bb15c323f222c412
Chang Xu <920114525@qq.com>
2019-08-02 02:00:22 -0700

    syntax checked

--- sources_1/new/ALU.v
+++ sources_1/new/ALU.v
@@ -36,12 +36,12 @@ module ALU(
     output reg[31:0] Z
     );
     
-    wire [31:0] A_in, B_in, Z_add_sub, Z_shift, Z_and, Z_or, Z_xor, Z_slt, Z_sltu;
+    wire [31:0] A_in_PC, B_in_PC, A_in, B_in, Z_add_sub, Z_shift, Z_and, Z_or, Z_xor, Z_slt, Z_sltu;
     wire EQ = A_in == B_in;
     wire LT = $signed(A_in) <= $signed(B_in);
     wire LTU = A_in <= B_in;
     assign A_in = Asrc? PC : rs1_data;
-    assign B_in = jalr? 32'b4 : Bsrc? Imm : rs2_data;
+    assign B_in = jalr? 32'h4 : Bsrc? imm : rs2_data;
     assign Z_add_sub = sub? (A_in + B_in) : (A_in - B_in);
     assign Z_shift = shdir? (rs1_data << B_in[4:0]) : sra? (rs1_data >>> B_in[4:0]) : (rs1_data >> B_in[4:0]);
     assign Z_and = A_in & B_in;
@@ -49,6 +49,11 @@ module ALU(
     assign Z_xor = A_in ^ B_in;
     assign Z_slt = LT? 32'b1 : 32'b0;
     assign Z_sltu = LTU? 32'b1 : 32'b0;
+    
+    // branch target adder
+    assign A_in_PC = jalr? rs1_data : PC;
+    assign B_in_PC = imm;
+    assign BTA = A_in_PC + B_in_PC;
 
     always@(*)begin
         case (ALUOP)
