0d22a8dddd175183921b24827e6a06db2f031206
Chang Xu <920114525@qq.com>
2019-08-02 01:52:12 -0700

    Decoding unit refined

--- sources_1/new/ALU.v
+++ sources_1/new/ALU.v
@@ -21,6 +21,46 @@
 
 
 module ALU(
-
+    input [31:0] rs1_data,
+    input [31:0] rs2_data,
+    input [31:0] PC,
+    input [31:0] imm,
+    input [2:0] ALUOP,
+    input Asrc,
+    input Bsrc,
+    input sra,
+    input shdir,
+    input sub,
+    input jalr,
+    output [31:0] BTA,
+    output reg[31:0] Z
     );
+    
+    wire [31:0] A_in, B_in, Z_add_sub, Z_shift, Z_and, Z_or, Z_xor, Z_slt, Z_sltu;
+    wire EQ = A_in == B_in;
+    wire LT = $signed(A_in) <= $signed(B_in);
+    wire LTU = A_in <= B_in;
+    assign A_in = Asrc? PC : rs1_data;
+    assign B_in = jalr? 32'b4 : Bsrc? Imm : rs2_data;
+    assign Z_add_sub = sub? (A_in + B_in) : (A_in - B_in);
+    assign Z_shift = shdir? (rs1_data << B_in[4:0]) : sra? (rs1_data >>> B_in[4:0]) : (rs1_data >> B_in[4:0]);
+    assign Z_and = A_in & B_in;
+    assign Z_or = A_in | B_in;
+    assign Z_xor = A_in ^ B_in;
+    assign Z_slt = LT? 32'b1 : 32'b0;
+    assign Z_sltu = LTU? 32'b1 : 32'b0;
+
+    always@(*)begin
+        case (ALUOP)
+            3'b000: Z = Z_add_sub;
+            3'b001: Z = Z_shift;
+            3'b010: Z = Z_slt;
+            3'b011: Z = Z_sltu;
+            3'b100: Z = Z_xor;
+            3'b101: Z = Z_shift; 
+            3'b110: Z = Z_or;
+            3'b111: Z = Z_and;
+        endcase 
+    end
+    
 endmodule
--- sources_1/new/DecodingUnit.v
+++ sources_1/new/DecodingUnit.v
@@ -22,61 +22,79 @@
 
 module DecodingUnit(
     input [31:0]IFQ_Instr,
+    output DU_rs1_valid,
+    output DU_rs2_valid,
     output [4:0]DU_rs1,
     output [4:0]DU_rs2,
     output [4:0]DU_rd,
     output DU_memread,
     output DU_memwrite,
-    output DU_sra_sub,
-    output [4:0]DU_shamt,
+    output DU_regwrite,
     output DU_j,
     output DU_br,
-    output [2:0]DU_ALUOP, 
-    output DU_regwrite,
+    output DU_jalr,
+    output DU_sub,
+    output DU_sra,
+    output DU_shdir,
+    output DU_funct3,
+    output DU_Asrc,
+    output DU_Bsrc,
+    output [2:0]DU_ALUout,
     output reg [31:0]DU_imm
     );
 
     reg raw_regwrite;
     wire[6:0] opcode = IFQ_Instr[6:0];
     wire[6:0] funct7 = IFQ_Instr[31:25];
-    assign DU_ALUOP[2:0] = IFQ_Instr[14:12];
+    wire[2:0] funct3 = IFQ_Instr[14:12];
+    wire LUI = opcode == 7'b0110111;
+    wire AUIPC = opcode == 7'b0010111;
+    wire JAL = opcode == 7'b1101111;
+    wire JALR = opcode == 7'b1100111;
+    wire B_type = opcode == 7'b1100011;
+    wire R_type = opcode == 7'b0110011;
+    wire I_type = opcode == 7'b0010011; // excluding LW and JALR
+    wire L_type = opcode == 7'b0000011;
+    wire S_type = opcode == 7'b0100011;
+
+    assign DU_ALUout[2:0] = (I_type || R_type)? funct3 : 3'b0; // used only when R-type and corresponding I-type
     assign DU_rd = IFQ_Instr[11:7];
-    assign DU_rs1 = IFQ_Instr[19:15];
+    assign DU_rs1 = LUI? 5'b0 : IFQ_Instr[19:15];
     assign DU_rs2 = IFQ_Instr[24:20];
-    assign DU_shamt = IFQ_Instr[24:20];
-    assign DU_sra_sub = funct7 == 7'b0100000;
-    assign DU_memread = opcode == 7'b0000011;
-    assign DU_memwrite = opcode == 7'b0100011;
-    assign DU_j = opcode == 7'b1101111 || opcode == 7'b1100111;
-    assign DU_br = opcode == 7'b1100011;
+    assign DU_rs1_valid = ~(LUI || AUIPC || JAL);
+    assign DU_rs2_valid = (B_type || S_type || R_type);
+    assign DU_sra = funct7 == 7'b0100000;
+    assign DU_shdir = funct3 == 3'b001; // shift left
+    assign DU_sub = (funct7 == 7'b0100000) & R_type;
+    assign DU_memread = L_type;
+    assign DU_memwrite = S_type;
+    assign DU_j = JAL || JALR;
+    assign DU_jalr = JALR;
+    assign DU_br = B_type;
     assign DU_regwrite = raw_regwrite && (DU_rd != 5'b0);
+    assign DU_Asrc = AUIPC || JAL || JALR; // 1: PC 0: (Rs1)
+    assign DU_Bsrc = ~(R_type || B_type); // 1: Imm (including PC + 4 for jalr) 0: (Rs2)
+    assign DU_funct3 = funct3;
 
     always@(*) begin
         raw_regwrite = 0;
         DU_imm = {IFQ_Instr[31:12], 12'b0};
-        if (opcode == 7'b0110111 || opcode == 7'b0010111) begin
-        // U-type
+        if (LUI || AUIPC) begin // U-type
             raw_regwrite = 1;
             // DU_imm = {IFQ_Instr[31:12], 12'b0}; default value
-        end else if (opcode == 7'b1101111) begin
-        // J-type jal
+        end else if (JAL) begin // J-type jal
             raw_regwrite = 1;
             DU_imm = {{12{IFQ_Instr[31]}}, IFQ_Instr[19:12], IFQ_Instr[20], IFQ_Instr[30:25], IFQ_Instr[24:21], 1'b0};
-        end else if (opcode == 7'b1100011) begin
-        // B-type
+        end else if (B_type) begin // B-type
             DU_imm = {{20{IFQ_Instr[31]}}, IFQ_Instr[7], IFQ_Instr[30:25], IFQ_Instr[11:8], 1'b0};
-        end else if (opcode == 7'b0100011) begin
-        // S-type
+        end else if (S_type) begin // S-type
             DU_imm = {{20{IFQ_Instr[31]}}, IFQ_Instr[31:25], IFQ_Instr[11:7]};
-        end else if (opcode == 7'b0000011 || opcode == 7'b0010011 || opcode == 7'b1100111) begin
-        // I-type (LW or ADDI or JALR)
+        end else if (L_type || I_type || JALR) begin // I-type (LW or ADDI or JALR)
             raw_regwrite = 1;
             DU_imm = {{20{IFQ_Instr[31]}}, IFQ_Instr[31:20]};
-        end else if (opcode == 7'b0110011) begin
-        // R-type
+        end else if (R_type) begin // R-type
             raw_regwrite = 1;
         end
-
     end
 
 endmodule
